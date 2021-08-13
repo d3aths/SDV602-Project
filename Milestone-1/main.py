51f20a38b2371d359a545c7a389a2e95c4b94d4a
d3aths <feels.blindx@gmail.com>
2021-08-13 20:30:19 +1200

    setting up main page

--- Milestone-1/main.py
+++ Milestone-1/main.py
@@ -1,14 +1,18 @@
-import PySimpleGUI as sg
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import PySimpleGUIQt as sg2
+import PySimpleGUI as sg
 import matplotlib
+import matplotlib.pyplot
+from numpy.core.numeric import binary_repr
 
-fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
+sg.theme('DarkGrey14')
+
+fig = matplotlib.pyplot.Figure(figsize=(5, 4), dpi=100)
 t = np.arange(0, 3, .01)
 fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 
-matplotlib.use("TkAgg")
+matplotlib.use('TkAgg')
+matplotlib.pyplot.ion()
 
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
@@ -18,9 +22,14 @@ def draw_figure(canvas, figure):
 
 # Define the window layout
 layout = [
-    [sg.Text("Plot test")],
+    [sg.Text("Graph 1")],
     [sg.Canvas(key="-CANVAS-")],
-    [sg.Button("Ok")],
+    [sg.Text("Summary of data", size=(40, 5))],
+    [sg.Button("Prev", size=(3,1)), sg.Button("Next", size=(3,1))],
+    [sg.Output(size=(61, 10), font=('Fira 12'))],
+    [sg.Multiline(size=(40, 3), enter_submits=True, key='query', do_not_clear=False),
+    sg.Button('Send', bind_return_key=True)],
+    [sg.Button("Close")],
 ]
 
 # Create the form and show it without the plot
@@ -31,12 +40,22 @@ window = sg.Window(
     finalize=True,
     element_justification="center",
     font="Helvetica 18",
+    default_button_element_size=(8,2),
+    use_default_focus=False
 )
 
 # Add the plot to the window
 draw_figure(window["-CANVAS-"].TKCanvas, fig)
 
-event, values = window.read()
+#Live chat event
+
+while True:     # The Event Loop
+    event, value = window.read()
+    if event in (sg.WIN_CLOSED, 'Close'):            # quit if exit button or X
+        break
+    if event == 'Send':
+        query = value['query'].rstrip()
+        # EXECUTE YOUR COMMAND HERE
+        print('Celeste: {}'.format(query), flush=True)
 
 window.close()
-
